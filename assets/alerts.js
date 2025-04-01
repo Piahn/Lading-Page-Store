384fea22fb4c1591ccfbec4675d8785c8bef63b2
PiahnVCp <owijaya131@gmail.com>
2025-04-01 15:04:22 +0700

    feat: lading page

---
+++ assets/alerts.js
@@ -0,0 +1,47 @@
+let imageContainer = document.getElementById("image-container");
+let ImageText = "";
+
+for (const review of image) {
+  ImageText += `<a href="javascript:void(0);" id="${review.id}" target="_blank"
+  class="w-full transition duration-300 aspect-video hover:rotate-3">
+  <img src="${review.image}" alt=""
+      class="object-cover object-center w-full border-8 border-white shadow-lg aspect-video">
+</a>`;
+}
+
+imageContainer.innerHTML = ImageText;
+
+image.map((review) => {
+  document.getElementById(review.id).addEventListener("click", function (e) {
+    e.preventDefault();
+
+    Swal.fire({
+      title: review.title,
+      html: `<div class="flex flex-col gap-4">
+          <div class="flex flex-col gap-1">
+              <span>${review.content}</span>
+          </div>
+          <div class="flex flex-row items-center gap-2">
+              <span class="text-sm">Dibuat oleh ${review.owner}</span>
+              <div class="flex flex-row items-center gap-1">
+              </div>
+          </div>
+      </div>
+      `,
+      showConfirmButton: true,
+      confirmButtonText: "Tutup",
+      confirmButtonColor: "#3b82f6",
+      showClass: {
+        popup: "come-from-behind",
+      },
+      hideClass: {
+        popup: "go-to-front",
+      },
+      didOpen: () => {
+        // Menambahkan efek tambahan saat alert terbuka
+        document.querySelector(".swal2-popup").style.transformStyle =
+          "preserve-3d";
+      },
+    });
+  });
+});
---
+++ data/reviews.js
@@ -0,0 +1,84 @@
+/* ----------------------------------------[Data Review]------------------------------ */
+
+// silakan di ubah sesuai kebutuhan
+let reviews = [
+  {
+    name: "Piahn",
+    job: "Programmer & Desainer",
+    comment:
+      "Ayo komen disini, ramaikan komentar dan review kamu tentang jasa ini. hohohoho.",
+    image: "https://pomf2.lain.la/f/3mwqt5ob.jpeg",
+  },
+  {
+    name: "Bahrul Alam",
+    job: "Senior Video Grapher",
+    comment:
+      "Mantap sekali jasanya, saya pesan logo kemaren besoknya langsung jadi sesuai permintaan lagi tanpa revisi",
+    image: "https://pomf2.lain.la/f/x5gop48e.jpeg",
+  },
+  {
+    name: "Naswya Yumna",
+    job: "Programmer",
+    comment:
+      "Aplikasi yang dibuat sangatlah mantap, tanpa bug dan clean code, mudah untuk di mainetenance serta user interfacenya menarik",
+    image: "https://pomf2.lain.la/f/lt31m41.jpeg",
+  },
+  {
+    name: "Setiyono",
+    job: "Dosen",
+    comment:
+      "Saya pesan aplikasi sosial media dosen, itu sangat bagus dan ada notifikasi di aplikasinya kaya aplikasi facebook tapi versi lokalnya",
+    image: "https://pomf2.lain.la/f/7hjqsru6.jpeg",
+  },
+  {
+    name: "Ilham Sumanto",
+    job: "Calon DPRD",
+    comment:
+      "Mantap desain kampanye saya udah jadi dan sudah dipasang, sekarang orang-orang sudah mengenal saja dan saya bisa lolos ke DPR",
+    image: "https://pomf2.lain.la/f/be0tckdb.jpeg",
+  },
+  {
+    name: "Adam Samantono",
+    job: "Profesor",
+    comment:
+      "Mantap dah, gw beli jasa desain infografis saya semakin menarik gara gara dibuatkan desain oleh kang TkJGrap. Pelanggan jadi makin rame dah.",
+    image: "https://pomf2.lain.la/f/jjp6s8bl.jpeg",
+  },
+];
+
+/* ----------------------------------------[Data Image]------------------------------ */
+
+let image = [
+  {
+    id: "test1",
+    title: "Informasi Penting",
+    content: "ini adalah informasi penting",
+    owner: "Fuzan",
+    alt: "tes",
+    image: "../assets/images/web-1.png",
+  },
+  {
+    id: "test2",
+    title: "Informasi Penting",
+    content: "ini adalah informasi penting",
+    owner: "Fuzan",
+    alt: "tes",
+    image: "../assets/images/web-2.png",
+  },
+  {
+    id: "test3",
+    title: "Informasi Penting",
+    content: "ini adalah informasi penting",
+    owner: "Fuzan",
+    alt: "tes",
+    image: "../assets/images/web-3.png",
+  },
+  {
+    id: "test4",
+    title: "Informasi Penting",
+    content: "ini adalah informasi penting",
+    owner: "Fuzan",
+    alt: "tes",
+    image: "../assets/images/web-4.png",
+  },
+];
